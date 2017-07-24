faf85c9b322aec45220d1d3b119db323da8eaa92
Clarissa Littler <clarissa.littler@gmail.com>
2017-07-24 09:49:59 -0700

    copy of simple markov chain example

---
+++ Markov1.py
@@ -0,0 +1,39 @@
+import sys
+import random
+
+numWords = int(sys.argv[1])
+dictFile = open(sys.argv[2],"r").read().split()
+
+def groupWords(text):
+    ourDict = {}
+    group = []
+    for w in text:
+        if len(group) == numWords:
+            if ourDict.get(tuple(group)) == None:
+                ourDict[tuple(group)] = [w]
+            else:
+                ourDict[tuple(group)].append(w)
+            group.append(w)
+            group = group[1:]
+        else:
+            group.append(w)
+    return ourDict
+
+def markovMulti(numTimes,ourDict):
+    outwords = []
+    testGroup = []
+    for i in range(0,numTimes):
+        if(len(testGroup) < numWords):
+            testGroup = list(random.choice(ourDict.keys()))
+            outwords = list(testGroup)
+        else:
+            w = random.choice(ourDict[tuple(testGroup)])
+            testGroup.append(w)
+            testGroup = testGroup[1:]
+            outwords.append(w)
+
+    return " ".join(outwords)
+
+d = groupWords(dictFile)
+
+print(markovMulti(1000,d)
